c33f55944b8184805559f03c8a94d553e532f199
navpreet-heer <64344650+navpreet-heer@users.noreply.github.com>
2020-08-23 16:40:45 +1000

    Add files via upload

---
+++ config.php
@@ -0,0 +1,18 @@
+<?php
+
+ $userRole = array(
+     0 => 'user',
+     1 => 'admin'
+ );
+
+ define('ES_USER_ROLE',$userRole);
+
+ $eventStaus = array(
+     0 => 'rejected',
+     1 => 'pending',
+     2=> 'approved'
+ );
+
+ define('EVENT_STATUS',$eventStaus);
+
+?>
