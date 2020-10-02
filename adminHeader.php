837173fb819e522ebd3e674235bf9a304cf0c296
navpreet-heer <64344650+navpreet-heer@users.noreply.github.com>
2020-10-02 22:54:16 +1000

    Add files via upload

--- adminHeader.php
+++ adminHeader.php
@@ -15,7 +15,7 @@
   <title>The Event Admin</title>
  
 
-  <base href="<?php echo DOMAIN;?>">
+  <base href="http://localhost/theevent/">
   <!-- Custom fonts for this template-->
   
   <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
