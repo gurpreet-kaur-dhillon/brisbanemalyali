bcda6594751471ff87ea60bbe5ec3a73f5d907f7
gurpreet-kaur-dhillon <64344370+gurpreet-kaur-dhillon@users.noreply.github.com>
2020-10-02 22:28:33 +1000

    Add files via upload

---
+++ DbConfig.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\data;
+
+abstract class Dbconfig{
+    protected $DB_HOST = 'localhost';
+    protected $DB_USER = 'root';
+    protected $DB_PASSWORD = '';
+    protected $DB_NAME = 'eventsite';  
+}
+
